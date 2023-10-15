3d14f6cd2a9d06049278e413387296d8cb821913
johnnyfsa <joaofeirense@gmail.com>
2023-10-15 11:20:42 -0300

    fixing animation issues

--- Assets/Scripts/RodeoClownAnimator.cs
+++ Assets/Scripts/RodeoClownAnimator.cs
@@ -1,21 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RodeoClownAnimator : MonoBehaviour
 {
+    private const string IDLE_SEXY_DANCE = "Idle_SexyDance";
+    private const string CAN_DANCE = "CanDance_b";
+    private const string IS_STATIC = "Static_b";
+    private const string SPEED = "Speed_f";
     private RodeoClownMovement rodeoClownMovement;
     private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rodeoClownMovement = GetComponentInParent<RodeoClownMovement>();
         animator = GetComponent<Animator>();
+        animator.SetBool(IS_STATIC, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //if the rodeClown is moving the animator should play the Run animation set the speed to 1 and it cannot dance
+        if (rodeoClownMovement.IsMoving())
+        {
+            animator.SetFloat(SPEED, 1);
+            animator.SetBool(CAN_DANCE, false);
+        }
+        //else if the rodeoClown is not moving the animator should set the speed to 0
+        else
+        {
+            animator.SetFloat(SPEED, 0);
+            //if the rodeoClown is not moving and canDance is true, than the rodeoClown should dance
+            if (rodeoClownMovement.CanDance)
+            {
+                animator.SetBool(CAN_DANCE, true);
+            }
+            //else if the rodeoclown is not moving and canDance is false, than the rodeoClown should not dance
+            else if (!rodeoClownMovement.CanDance)
+            {
+                animator.SetBool(CAN_DANCE, false);
+            }
+        }
     }
 }
--- Assets/Scripts/RodeoClownMovement.cs
+++ Assets/Scripts/RodeoClownMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,25 +16,37 @@ public class RodeoClownMovement : MonoBehaviour
     private float distanceToCenter;
     private Vector3 centerOfArena;
 
+    private bool canDance;
+
+    public bool CanDance { get => canDance; set => canDance = value; }
 
     private void Start()
     {
-        centerOfArena = new Vector3(Random.Range(-3f, 3f), transform.position.y, Random.Range(-3f, 3f));
+        centerOfArena = new Vector3(UnityEngine.Random.Range(-3f, 3f), transform.position.y, UnityEngine.Random.Range(-3f, 3f));
         //using the player name is a bad idea because it can change
         player = GameObject.Find("Player").GetComponent<Player>();
     }
     private void Update()
     {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        moveDir = Vector3.zero;
         //calculate distance to player
         distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+        //calculate the distance to the center of the arena
         distanceToCenter = Vector3.Distance(transform.position, centerOfArena);
         //if shorter than distanceToPlayer, run away from player
         if (distanceToPlayer < distanceThresholdToPlayer)
         {
+            //player is moving so it's better to run away, runing animation
             if (player.isMoving)
             {
                 RunAwayFromPlayer();
             }
+            //player is standing still so it's better to turn and face the player, idle animation
             else
             {
                 TurnToPlayer();
@@ -42,20 +55,30 @@ public class RodeoClownMovement : MonoBehaviour
         //if longer than distanceToPlayer, run to center of arena
         else
         {
+            /*the center of the arena has a radius of distanceThresholdToCenter, 
+            if it's out of the area defined by this radius, it should move towards the center*/
             if (distanceToCenter > distanceThresholdToCenter)
             {
                 RunToCenterOfArena();
             }
+            //if the enemy is inside the area defined by the radius, it should dance
+            else if (distanceToCenter <= distanceThresholdToCenter)
+            {
+                canDance = true;
+            }
         }
+
     }
     private void RunAwayFromPlayer()
     {
+        canDance = false;
         moveDir = transform.position - player.transform.position;
         transform.Translate(moveDir.normalized * speed * Time.deltaTime, Space.World);
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
     }
     private void RunToCenterOfArena()
     {
+        canDance = false;
         moveDir = centerOfArena - transform.position;
         transform.Translate(moveDir.normalized * speed * Time.deltaTime, Space.World);
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotationSpeed * Time.deltaTime);
@@ -67,5 +90,12 @@ public class RodeoClownMovement : MonoBehaviour
         transform.forward = Vector3.Slerp(transform.forward, playerDirection, rotationSpeed * Time.deltaTime);
     }
 
-
+    public bool IsMoving()
+    {
+        if (moveDir == Vector3.zero)
+        {
+            return false;
+        }
+        return true;
+    }
 }
